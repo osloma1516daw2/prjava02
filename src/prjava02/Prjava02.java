1b5975bb93b8e797a7c4fe357aecb8743e0a3ac3
osloma1516daw2 <osloma1516daw2@gmail.com>
2015-11-04 00:15:45 +0100

    Versió 0.1 del projecte prjava02

---
+++ src/prjava02/Prjava02.java
@@ -0,0 +1,25 @@
+package prjava02;
+
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author osloma1516daw2@gmail.com
+ */
+public class Prjava02 {
+
+    public static void main(String[] args) {
+        System.out.println("Versió 0.1 del projecte prjava02");
+        try{
+            InetAddress adreça = InetAddress.getLocalHost();
+            String hostname = adreça.getHostName();
+            System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+            System.out.println("Versió OS: " + System.getProperty("os.version"));
+        }
+        catch (IOException e){
+        }
+    }
+}
