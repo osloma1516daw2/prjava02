334579ba0b5d97859a52a843b565518c2bee1aee
osloma1516daw2 <osloma1516daw2@gmail.com>
2015-11-04 12:06:55 +0100

    Creant un primer conflicte amb la branca01

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -19,7 +19,8 @@ public class Prjava02 {
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió OS: " + System.getProperty("os.version"));
             System.out.println("Creació d'una branca del projecte prjava02");
-            System.out.println("Afegint més codi a la branca00 del projecte prjava02");  
+            System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+            System.out.println("Conflicte típic: Codi diferent a una mateixa línia");  
         }
         catch (IOException e){
         }
